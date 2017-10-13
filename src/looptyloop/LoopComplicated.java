6dba916b80adf3e51a526b43f9bb4df4e12bd140
Francisco6969 <francisco.estrella-rodriguez@ocsbstudent.ca>
2017-10-13 14:23:14 -0400

    FirstLoops

---
+++ src/looptyloop/LoopComplicated.java
@@ -0,0 +1,20 @@
+package looptyloop;
+import java.util.Random;
+import java.util.Scanner;
+public class LoopComplicated {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in=new Scanner(System.in);
+		Random rnd=new Random();
+
+		int hi;
+		int low; 
+		System.out.println("Hi, time to make a time table.");
+		System.out.println("What is the biggest number you want?");
+		hi=in.nextInt();
+		
+
+	}
+
+}
