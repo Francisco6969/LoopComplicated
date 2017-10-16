7821f3909919f13a1a0c20fad57adb244d3e6a69
Francisco6969 <francisco.estrella-rodriguez@ocsbstudent.ca>
2017-10-16 13:22:17 -0400

    DONELOOP

--- src/looptyloop/LoopComplicated.java
+++ src/looptyloop/LoopComplicated.java
@@ -12,21 +12,34 @@ public class LoopComplicated {
 		int low; 
 		int hi2=0;
 		System.out.println("Hi, time to make a time table.");
+		System.out.println("What is the smallest number you want?");
+		low=in.nextInt();
 		System.out.println("What is the biggest number you want?");
 		hi=in.nextInt();
-		System.out.println("What is the biggest number you want?");
-		low=in.nextInt();
 		if (hi<low) {
+			hi2=hi;
 			hi=low;
 			low=hi2;
 			System.out.println("Sorry but I was told to make this idiot proof");
 		}
-		while (hi<low) {
-		System.out.print(low);
-		while (hi<low) 
-			System.out.print(low++); 
+		System.out.print("\t");
+		for (int x=low;x<=hi;x++) { 
+			System.out.print(x+"\t");
+		
 		}
-			
-
+		System.out.println();
+		System.out.println("\t");
+		for (int y=low;y<=hi;y++) {
+			System.out.print(y+"\t");
+			for (int x=low;x<=hi;x++) {
+				System.out.print(x*y+"\t");
+			}
+			System.out.println();
+			System.out.println();
+		}
+		
 	}
+
+
 }
+
